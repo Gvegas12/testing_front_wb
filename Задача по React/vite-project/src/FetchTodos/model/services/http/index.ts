b10fb891aebae7f9cf851e047103aa49d34b3ad3
GvegasABDM <alex.abdm@yandex.ru>
2023-10-16 16:53:36 +0300

    f

---
+++ Задача по React/vite-project/src/FetchTodos/model/services/http/index.ts
@@ -0,0 +1,9 @@
+import { getAllTodos } from "./req/getAllTodos";
+import { getOneTodo } from "./req/getOneTodo";
+
+const TodoHttpService = {
+  getAllTodos,
+  getOneTodo,
+};
+
+export default TodoHttpService;
